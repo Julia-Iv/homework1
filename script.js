7362c04be5a205533d533705a5f19bbff38fc6f3
Julia <bogatayadomo@gmail.com>
2026-03-02 19:21:14 +0300

    hw2js

---
+++ script.js
@@ -0,0 +1,42 @@
+//задание 1
+let a = 10;
+alert (a);
+let b = 20;
+alert (b);
+
+//задание 2
+const c = 2007;
+alert (c);
+
+//задание 3
+const d = "Брендан Айк";
+alert (d);
+
+//задание 4
+let f = 10;
+let g = 2;
+let sum = f + g;
+alert (sum);
+let difference = f - g;
+alert (difference);
+let product = f * g;
+alert (product);
+let quotient = f / g;
+alert (quotient);
+
+//задание 5
+let result = 2 **5;
+alert (result);
+
+//задание 6
+let h = 9;
+let j = 2;
+let remainder = h % j;
+alert (remainder);
+
+//задание 7
+let num = 1;
+num += 5;
+num -= 3;
+num *= 7;
+alert (num);
