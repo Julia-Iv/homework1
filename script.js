942326f155eb85d0e01e0bb21421cbf3c15c7331
Julia <bogatayadomo@gmail.com>
2026-03-02 22:14:29 +0300

    hw2js

--- script.js
+++ script.js
@@ -5,12 +5,12 @@ let b = 20;
 alert (b);
 
 //задание 2
-const c = 2007;
-alert (c);
+const iphone = 2007;
+alert (iphone);
 
 //задание 3
-const d = "Брендан Айк";
-alert (d);
+const nameJS = "Брендан Айк";
+alert (nameJS);
 
 //задание 4
 let f = 10;
@@ -39,4 +39,25 @@ let num = 1;
 num += 5;
 num -= 3;
 num *= 7;
+num /= 3;
+num ++;
+num --;
 alert (num);
+
+//задание 8
+let age = prompt ("Сколько Вам лет?");
+alert (age);
+
+//задание 9
+const user = {
+name: 'Nikita',
+age: 17,
+isAdmin: false
+};
+console.log(user['name']);
+console.log(user['age']);
+console.log(user['isAdmin']);
+
+//задание 10
+let userName = prompt ('Ваше Имя?');
+alert(`Привет ${userName}!`);
