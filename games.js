a7d1cc592bde913d2101cd2734efd61ed5b21d89
Julia <bogatayadomo@gmail.com>
2026-03-20 00:17:04 +0300

    games-js1

--- games.js
+++ games.js
@@ -3,13 +3,15 @@ function miniGames1() {
     while (true) {
         let guessNumber = prompt ("Угадай число от 1 до 100");
         guessNumber = Number(guessNumber);
-        if (guessNumber === randomNumber) {
+         if (guessNumber === randomNumber) {
             alert ("Молодец! Ты угадал числою");
             break;
         } else if (guessNumber > randomNumber) {
             alert ("Твое число больше загаданного, попробуй еще угадать!");
-        } else {
+        } else if (guessNumber < randomNumber) {
             alert ("Твое число меньше загаданного, попробуй еще угадать!");
+        }else if (guessNumber === null) {
+            return;
         }
 
     }
