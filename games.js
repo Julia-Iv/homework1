ef0890cc34ff79bd92e8747ec1c7aed2922c089a
Julia <bogatayadomo@gmail.com>
2026-03-21 14:58:31 +0300

    add null

--- games.js
+++ games.js
@@ -1,18 +1,24 @@
 function miniGames1() {
     let randomNumber = Math.floor(Math.random() * 100) + 1;
+    //if (randomNumber === null) {
+      //      return;
+        //}
     while (true) {
         let guessNumber = prompt ("Угадай число от 1 до 100");
+        if (guessNumber === null) {
+            return;
+        }
         guessNumber = Number(guessNumber);
-         if (guessNumber === randomNumber) {
+        if (guessNumber === randomNumber) {
             alert ("Молодец! Ты угадал числою");
-            break;
         } else if (guessNumber > randomNumber) {
             alert ("Твое число больше загаданного, попробуй еще угадать!");
         } else if (guessNumber < randomNumber) {
             alert ("Твое число меньше загаданного, попробуй еще угадать!");
-        }else if (guessNumber === null) {
-            return;
         }
-
     }
+    //if (guessNumber === null) {
+      //      return;
+       // } 
 }
+
