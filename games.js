317e17a98afbe376df0e8c7466310d88bd0c42ac
Julia <bogatayadomo@gmail.com>
2026-03-28 00:12:21 +0300

    games2

--- games.js
+++ games.js
@@ -19,16 +19,12 @@ function miniGames1() {
         }
     }
 }
-
- // Функция генерации случайного числа в заданном диапазоне
- function getRandomInt(min, max) {
- return Math.Floor(Math.Random() * (max - min + 1)) + min;
-}
-
- // Функция создания примера
  function miniGames2() {
+    function getRandomInt() {
+          return Math.floor(Math.random() * 50);
+ }
  const ops = ['+', '-', '*', '/'];
- const op = ops[getRandomInt(0, 3)];
+ const op = ops[getRandomInt()];
  let a, b;
  let userAnswer = prompt(`Сколько будет ${op}?`);
  
@@ -58,7 +54,7 @@ function miniGames1() {
 
  const question = `${a} ${op} ${b}`;
  const correctAnswer = eval(question);
- 
+
  return {
  question: question,
  answer: correctAnswer
@@ -70,55 +66,4 @@ if (question === correctAnswer){
  }
 
 }
-/*
- // Основная функция проверки ответа
- function checkAnswer() {
- const userInput = document.GetElementById('userAnswer').Value;
- const {question, answer} = createQuestion();
 
- // Обновляем пример
- document.GetElementById('question').TextContent = `Решите пример: ${question}`;
-
- if (userInput === '') {
- document.GetElementById('result').TextContent ='jjjjk';
- }
-}
-//const sign = ['+', '-', '*', '/'];
-//const minNumber = 1;
-//const maxNumber = 50;
-//генерация случайной задачи
-//function miniGames2() {
-//const sign1 = sign[Math.floor(Math.Random() * sign.Length)];
-//let a, b, result;
- //if (sign1 === '+') {
- //a = Math.Floor(Math.Random() * (maxNumber - (minNumber + 1))) + minNum;
- //b = Math.Floor(Math.Random() * (maxNum - (minNum + 1))) + minNum;
- /*result = a + b;
-} else if (sign1 === '-') {
- a = Math.Floor(Math.Random() * (maxNum - (minNum + 1))) + minNum;
- b = Math.Floor(Math.Random() * (a - (minNum + 1)) + minNum ) ; // b ≤ a
- result = a - b;
-} else if (sign1 === '*') {
- a = Math.Floor(Math.Random() * (maxNum - (minNum + 1))) + minNum;
- b = Math.Floor(Math.Random() * (maxNum - (minNum + 1))) + minNum;
- result = a * b;
-} else if (sign1 === '/') {
- // Чтобы результат был целым
- a = Math.Floor(Math.Random() * (maxNum - (minNum + 1))) + minNum;
- b = Math.Floor(Math.Random() * (a / minNum)) + 1; // b ≥ 1, a делится на b
- result = a / b;
-}
-let result = 
-}
-
-//function getRandomElement(array) {
-//const randomIndex = Math.floor(Math.random() * array.length);
-//return array[randomIndex];
-//}
-for (let i = 0; i < sign.length; i++){
-    sign.push(miniGames2());
-}
-console.log(miniGames2(sign));
-
-//const fruits = ["🍎", "🍌", "🍊", "🍇", "🍓"];
-//console.log(getRandomElement(fruits)); // Может вернуть любой фрукт*/
